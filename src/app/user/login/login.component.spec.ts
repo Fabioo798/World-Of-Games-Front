7ecc4a5406aa4c5c5fcecf42facc7daa1d0e283c
Fabio <fabiodn.798@gmail.com>
2023-03-23 09:36:49 +0100

    ✅ Create login component

--- src/app/user/login/login.component.spec.ts
+++ src/app/user/login/login.component.spec.ts
@@ -1,17 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
 import { LoginComponent } from './login.component';
+import { repoService, User } from 'src/app/services/register.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockRepoService: jasmine.SpyObj<repoService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+  beforeEach(async(() => {
+    mockRepoService = jasmine.createSpyObj(['loginUser']);
 
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule, RouterTestingModule],
+      providers: [{ provide: repoService, useValue: mockRepoService }],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +35,56 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call loginUser on form submission', fakeAsync(() => {
+    // Arrange
+    const user: User = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password',
+      img: '',
+    };
+    const token = 'mock-token';
+    mockRepoService.loginUser.and.returnValue(of({ results: token }));
+    spyOn(component.onAdd, 'emit');
+
+    // Act
+    component.login.controls['email'].setValue(user.email);
+    component.login.controls['password'].setValue(user.password);
+    component.handleSubmit();
+    tick(3000);
+
+    // Assert
+    expect(mockRepoService.loginUser).toHaveBeenCalledWith(
+      { email: user.email, password: user.password }
+      //
+    );
+    expect(component.token).toBe(token);
+    expect(component.onAdd.emit).toHaveBeenCalledWith({ email: user.email });
+    expect(component.isSuccess).toBeTruthy();
+    expect(component.login.value).toEqual({ email: '', password: '' });
+  }));
+
+  it('should set isError to true when loginUser returns an error', fakeAsync(() => {
+    // Arrange
+    const user: User = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password',
+      img: '',
+    };
+    mockRepoService.loginUser.and.returnValue(
+      throwError('Error logging in user')
+    );
+
+    // Act
+    component.login.controls['email'].setValue(user.email);
+    component.login.controls['password'].setValue(user.password);
+    component.handleSubmit();
+    tick(3000);
+
+    // Assert
+    expect(component.isLoading).toBe(false);
+    expect(component.isError).toBe(false);
+  }));
 });
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User, repoService } from 'src/app/services/register.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  login: FormGroup;
+  @Output() onAdd: EventEmitter<Partial<User>>;
+  token: string;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  constructor(
+    public form: FormBuilder,
+    public srv: repoService,
+    private router: Router
+  ) {
+    this.onAdd = new EventEmitter();
+    this.login = form.group({
+      email: '',
+      password: '',
+    });
+    this.token = '';
+    this.isLoading = false;
+    this.isSuccess = false;
+    this.isError = false;
+  }
 
+  handleSubmit() {
+    const sendLogUser: Partial<User> = {
+      email: this.login.value.email,
+      password: this.login.value.password,
+    };
+    console.log(sendLogUser);
+    this.srv.loginUser(sendLogUser).subscribe((response: any) => {
+      console.log(response);
+      console.log('user logged');
+      this.token = response.results.token; // Store the token
+      console.log('Token:', this.token);
+      localStorage.setItem('token', this.token); // Save the token to the local storage
+      this.isSuccess = true;
+        setTimeout(() => {
+          this.isSuccess = false;
+          this.router.navigateByUrl('/home');
+        }, 2000);
+    },
+    (error: any) => {
+      console.log(error);
+      this.isLoading = false;
+      this.isError = true;
+      setTimeout(() => {
+        this.isError = false;
+      }, 2000);
+    });
+    this.login.reset();
+  }
+
+  // loadUsers() {
+  //   const headers = { Authorization: `Bearer ${this.token}` };
+  //   this.srv.loadUser(headers).subscribe((users: User[]) => {
+  //     console.log('Users:', users);
+  //   });
+  // }
 }
