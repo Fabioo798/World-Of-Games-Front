343047f3cc444e2646207c05bd196edbdb6c1e4f
Fabio <fabiodn.798@gmail.com>
2023-03-23 09:40:49 +0100

    🚧 Create user service

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export type User = {
+  name: string;
+  email: string;
+  password: string;
+  img: string;
+};
+
+export type apiAnswer = {
+  result: any;
+};
+@Injectable({
+  providedIn: 'root',
+})
+export class repoService {
+  private apiUrl = 'http://localhost:4800/users/register';
+
+  constructor(private http: HttpClient) {}
+
+  registerUser(user: User): Observable<any> {
+    return this.http.post('http://localhost:4800/users/register', user);
+  }
+
+  loginUser(user: Partial<User>): Observable<any> {
+    return this.http.post('http://localhost:4800/users/login', user);
+  }
+  loadUser(headers?: any): Observable<User[]> {
+    const options = { headers };
+    return this.http.get<User[]>('http://localhost:4800/users/', options);
+  }
+}
---
+++ src/app/services/user/user.spec.ts
@@ -0,0 +1,86 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { repoService, User } from './user.service';
+
+describe('repoService', () => {
+  let service: repoService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [repoService],
+    });
+    service = TestBed.inject(repoService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('registerUser', () => {
+    it('should register a new user', () => {
+      const user: User = {
+        name: 'Test User',
+        email: 'test@test.com',
+        password: 'password',
+        img: 'test.jpg',
+      };
+
+      service.registerUser(user).subscribe((result) => {
+        expect(result).toBeDefined();
+      });
+
+      const req = httpMock.expectOne('http://localhost:4800/users/register');
+      expect(req.request.method).toEqual('POST');
+      req.flush({ result: true });
+    });
+  });
+
+  describe('loginUser', () => {
+    it('should login a user', () => {
+      const user: Partial<User> = {
+        email: 'test@test.com',
+        password: 'password',
+      };
+
+      service.loginUser(user).subscribe((result) => {
+        expect(result).toBeDefined();
+      });
+
+      const req = httpMock.expectOne('http://localhost:4800/users/login');
+      expect(req.request.method).toEqual('POST');
+      req.flush({ result: true });
+    });
+  });
+
+  describe('loadUser', () => {
+    it('should load user data', () => {
+      const headers = { Authorization: 'Bearer token' };
+
+      service.loadUser(headers).subscribe((users) => {
+        expect(users.length).toBeGreaterThan(0);
+      });
+
+      const req = httpMock.expectOne('http://localhost:4800/users/');
+      expect(req.request.method).toEqual('GET');
+      expect(req.request.headers.get('Authorization')).toEqual('Bearer token');
+      req.flush([
+        {
+          name: 'Test User',
+          email: 'test@test.com',
+          password: 'password',
+          img: 'test.jpg',
+        },
+      ]);
+    });
+  });
+});
