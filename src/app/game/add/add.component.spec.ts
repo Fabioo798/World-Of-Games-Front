b7bdff7735b33745acd67fdf648cd949fcbe9d8e
Fabio <fabiodn.798@gmail.com>
2023-03-28 16:12:05 +0200

    ➕ Create add component

--- src/app/game/add/add.component.spec.ts
+++ src/app/game/add/add.component.spec.ts
@@ -5,19 +5,31 @@ import {
   TestBed,
   tick,
 } from '@angular/core/testing';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideStorage } from '@angular/fire/storage';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, StorageReference } from 'firebase/storage';
 import { of, throwError } from 'rxjs';
 import { RepoGameService } from 'src/app/services/game/game.services.service';
+import { Game } from 'src/app/types/types';
 import { UserDetailComponent } from 'src/app/user/detail/detail.component';
-import { mockGameService, mockToken, mockUser } from 'src/app/utils/mocks';
-
+import {
+  mockGame,
+  mockGameService,
+  mockGametoUp,
+  mockUser,
+} from 'src/app/utils/mocks';
+import { environment } from 'src/environments/environment';
 import { AddComponent } from './add.component';
 
 describe('AddComponent', () => {
   let component: AddComponent;
   let fixture: ComponentFixture<AddComponent>;
   let srv: RepoGameService;
+  let mockActivatedRoute: any;
+  let gameToUpdate: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -29,6 +41,8 @@ describe('AddComponent', () => {
         RouterTestingModule.withRoutes([
           { path: 'user/:id', component: UserDetailComponent },
         ]),
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideStorage(() => getStorage()),
       ],
       providers: [
         {
@@ -37,10 +51,12 @@ describe('AddComponent', () => {
         },
       ],
     }).compileComponents();
-
+  });
+  beforeEach(() => {
     fixture = TestBed.createComponent(AddComponent);
     component = fixture.componentInstance;
     srv = TestBed.inject(RepoGameService);
+    component.gameToUpdate = {} as unknown as Game;
     fixture.detectChanges();
   });
 
@@ -48,21 +64,80 @@ describe('AddComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('Given the handleSubmit method', () => {
+  describe('(UPDATE)When we have a game to update', () => {
+    it('Should call the RepoGameService service and next', fakeAsync(() => {
+      component.newGame.value['gameName'] = 'TestName';
+      component.newGame.value['releaseDate'] = 'TestReleaseDate';
+      component.newGame.value['category'] = 'TestCategory';
+      component.newGame.value['price'] = 20;
+      component.newGame.value['description'] = 'TestDescription';
+      component.newGame.value['img'] = 'TestImg';
+      const spynewGame = spyOn(srv, 'updateGame').and.returnValue(
+        of(mockGametoUp)
+      );
+      const spyZoneRun = spyOn(component.zone, 'run').and.callThrough();
+      const mockEvent = {
+        target: {
+          files: [
+            {
+              name: 'test',
+              size: 0,
+              type: 'image/png',
+            },
+          ],
+        },
+      };
+      srv.gameInfo$.next(mockGametoUp);
+      component.saveImage(mockEvent);
+      const spyUpload = spyOn(component, 'uploadImage').and.resolveTo();
+      const spyGetImage = spyOn(component, 'getImage').and.resolveTo('mock');
+      component.ngOnInit();
+      component.handleSubmit();
+      tick(2000);
+      expect(spyUpload).toHaveBeenCalled();
+      expect(spyGetImage).toHaveBeenCalled();
+
+      expect(spynewGame).toHaveBeenCalled();
+      expect(spyZoneRun).toHaveBeenCalled();
+      expect(component.isSuccess).toBeFalse();
+      expect(component.isError).toBeFalse();
+      expect(component.router.url).toBe('/user/:id');
+    }));
+  });
+  describe('(CREATE)Given the handleSubmit method', () => {
     describe('When called with correct data', () => {
       it('Should call the RepoGameService service and next', fakeAsync(() => {
         component.newGame.value['gameName'] = 'TestName';
         component.newGame.value['releaseDate'] = 'TestReleaseDate';
         component.newGame.value['category'] = 'TestCategory';
-        component.newGame.value['price'] = 'TestPrice';
+        component.newGame.value['price'] = 20;
         component.newGame.value['description'] = 'TestDescription';
         component.newGame.value['img'] = 'TestImg';
         const spynewGame = spyOn(srv, 'createGame').and.returnValue(
-          of(mockUser)
+          of(mockGametoUp)
         );
         const spyZoneRun = spyOn(component.zone, 'run').and.callThrough();
+        const mockEvent = {
+          target: {
+            files: [
+              {
+                name: 'test',
+                size: 0,
+                type: 'image/png',
+              },
+            ],
+          },
+        };
+        debugger;
+        srv.gameInfo$.next({} as unknown as Game);
+        component.gameToUpdate = {} as any;
+        component.saveImage(mockEvent);
+        const spyUpload = spyOn(component, 'uploadImage').and.resolveTo();
+        const spyGetImage = spyOn(component, 'getImage').and.resolveTo('mock');
         component.handleSubmit();
         tick(2000);
+        expect(spyUpload).toHaveBeenCalled();
+        expect(spyGetImage).toHaveBeenCalled();
         expect(spynewGame).toHaveBeenCalled();
         expect(spyZoneRun).toHaveBeenCalled();
         expect(component.isSuccess).toBeFalse();
@@ -79,13 +154,73 @@ describe('AddComponent', () => {
         component.newGame.value['price'] = 'TestPrice';
         component.newGame.value['description'] = 'TestDescription';
         component.newGame.value['img'] = 'TestImg';
-        const errorResponse = { error: { message: 'Error message' } };
         spyOn(srv, 'createGame').and.returnValue(throwError(() => 'error'));
+        const mockEvent = {
+          target: {
+            files: [
+              {
+                name: 'test',
+                size: 0,
+                type: 'image/png',
+              },
+            ],
+          },
+        };
+        srv.gameInfo$.next(null as unknown as Game);
+        component.gameToUpdate = null as any;
+        component.saveImage(mockEvent);
+        const spyUpload = spyOn(component, 'uploadImage').and.resolveTo();
+        const spyGetImage = spyOn(component, 'getImage').and.resolveTo('mock');
+
         component.handleSubmit();
-        tick(2000);
+        tick(3000);
+
+        expect(spyUpload).toHaveBeenCalled();
+        expect(spyGetImage).toHaveBeenCalled();
         expect(component.isSuccess).toBeFalse();
         expect(component.isError).toBeFalse();
       }));
+    });
+
+    describe('When the getImage method is called', () => {
+      it('then it should return the string', () => {
+        const mockStorage = {} as StorageReference;
+        const spyGet = spyOn(component, 'getImage').and.callFake(() => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          return new Promise((_resolve, _reject) => {
+            return '';
+          });
+        });
+        component.getImage(mockStorage);
+        expect(spyGet).toHaveBeenCalled();
+      });
+    });
+
+    describe('When the uploadImage method is called', () => {
+      it('Then it should uploadBytes', async () => {
+        const testRef = ref(component['storage'], 'testing');
+        const file = new File(['test'], 'test.png', { type: 'image/png' });
+
+        spyOn(component, 'uploadImage').and.callThrough();
+
+        await component.uploadImage(testRef, file);
+
+        const downloadUrl = await component.getImage(testRef);
+
+        expect(component.uploadImage).toHaveBeenCalled();
+        expect(downloadUrl).toBeTruthy();
+      });
+    });
+    describe('submitButtonLabel', () => {
+      it('should return "Create" when gameToUpdate is falsy', () => {
+        component.gameToUpdate = null as unknown as Game;
+        expect(component.submitButtonLabel).toEqual('Create');
+      });
+
+      it('should return "Save" when gameToUpdate is truthy', () => {
+        component.gameToUpdate = { id: '1', gameName: 'Test Game' } as Game;
+        expect(component.submitButtonLabel).toEqual('Save');
+      });
     });
   });
 });
--- src/app/game/add/add.component.ts
+++ src/app/game/add/add.component.ts
@@ -1,10 +1,24 @@
-import { Component, EventEmitter, Input, NgZone, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  NgZone,
+  Output,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { RepoGameService } from 'src/app/services/game/game.services.service';
 import { Game } from 'src/app/types/types';
 import { ActivatedRoute } from '@angular/router';
+import {
+  getDownloadURL,
+  ref,
+  Storage,
+  StorageReference,
+  uploadBytes,
+} from '@angular/fire/storage';
 
 @Component({
   selector: 'app-add',
@@ -17,7 +31,9 @@ export class AddComponent {
   isSuccess: boolean;
   isError: boolean;
   isUpdate: boolean;
-  @Input() gameToUpdate: Game | undefined;
+  gameToUpdate: Game | undefined;
+  private storage: Storage;
+  private gameImg: File;
 
   constructor(
     public form: FormBuilder,
@@ -26,6 +42,8 @@ export class AddComponent {
     public zone: NgZone,
     public route: ActivatedRoute
   ) {
+    this.gameImg = new File([], '');
+    this.storage = inject(Storage);
     this.onAdd = new EventEmitter();
     this.newGame = form.group({
       gameName: ['', Validators.required],
@@ -40,12 +58,24 @@ export class AddComponent {
     this.isUpdate = false;
   }
 
+  saveImage(event: any) {
+    this.gameImg = event.target.files[0];
+  }
+
+  async uploadImage(storage: StorageReference, avatar: File): Promise<void> {
+    await uploadBytes(storage, avatar);
+  }
+
+  async getImage(storage: StorageReference): Promise<string> {
+    const avatar = await getDownloadURL(storage);
+    return avatar;
+  }
+
   ngOnInit() {
-    console.log(this.route.snapshot.paramMap.get('gameToUpdate'));
-    const gameToUpdateString = this.route.snapshot.paramMap.get('gameToUpdate');
-    this.gameToUpdate = JSON.parse(gameToUpdateString as string) as Game;
-    if (this.gameToUpdate) {
-      this.newGame.setValue({
+    this.gameToUpdate = this.srv.gameInfo$.value;
+    console.log(this.gameToUpdate !== {} as Game);
+    if (this.gameToUpdate !) {
+      this.newGame.patchValue({
         gameName: this.gameToUpdate.gameName,
         releaseDate: this.gameToUpdate.releaseDate,
         category: this.gameToUpdate.category,
@@ -54,24 +84,36 @@ export class AddComponent {
         img: '',
       });
       this.isUpdate = true;
+      this.srv.gameInfo$.next({} as Game);
     }
   }
 
-  handleSubmit() {
+  async handleSubmit() {
+    let gameImg = '';
+    let observable: Observable<any>;
+
+    if (this.gameImg.name) {
+      const avatarRef = ref(
+        this.storage,
+        `avatars/${this.newGame.value.email}`
+      );
+
+      await this.uploadImage(avatarRef, this.gameImg);
+
+      gameImg = await this.getImage(avatarRef);
+    }
     const sendNewGame: Game = {
       gameName: this.newGame.value.gameName,
       releaseDate: this.newGame.value.releaseDate,
       category: this.newGame.value.category,
       price: this.newGame.value.price,
       description: this.newGame.value.description,
-      img: this.newGame.value.img,
+      img: gameImg,
     };
 
-    let observable: Observable<any>;
-
-    if (this.gameToUpdate) {
+    if (this.isUpdate === true) {
       observable = this.srv.updateGame(
-        this.gameToUpdate.id as string,
+        this.gameToUpdate?.id as string,
         sendNewGame
       );
     } else {
